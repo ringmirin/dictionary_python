cb54eb005e452a82822c7e4d2152c0a4f7f597d9
ringmirin <ringmirin21@navgurukul.org>
2022-07-28 23:58:41 +0530

    first commit

---
+++ alphabetical order.py
@@ -0,0 +1,12 @@
+# r={'list1': ['b', 'a', 'c'], 'list2': ['f', 'a', 'e'], 'list3': ['c', 'b', 'a']}
+
+# s=sorted(r: )
+
+# dict={}
+# list=eval(input("enter the aplhabet: "))
+# sorted.list
+# for i in range(len(list)):
+#    dict[i+1]=list[i]
+# print(dict)
+
+
---
+++ ascending_descending.py
@@ -0,0 +1,22 @@
+# Write a Python program to sort (ascending and descending) a dictionary by value
+
+# my_dict={"c":3,"a":1,"g":7,"d":4}
+# l=list(my_dict.items())
+# l.sort()
+# dict=dict(l)
+# print(l)
+# m=list(my_dict.items())
+# l.sort(reverse=True)
+# dict=dict(l)
+# print(dict)
+
+
+
+my_dict={"c":3,"a":1,"g":7,"d":4}
+# sorted_value=sorted(my_dict.values())
+# dict1={}
+# for i in sorted_value:
+#    for k in my_dict.keys():
+#       if my_dict[k]==i:
+#          dict1[k]=my_dict[k]
+# print(dict1)
