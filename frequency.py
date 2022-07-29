857ca980096724ed6edf8c330fdf2c027c265a80
ringmirin <ringmirin21@navgurukul.org>
2022-07-29 21:21:20 +0530

    first commit

--- frequency.py
+++ frequency.py
@@ -2,12 +2,24 @@
 # # "MISSISSIPPI" to a dictionary, were the letters are the keys and their frequencies 
 # # are the values.
 
-# word="MISSISSIPPI"
-# freq={}
-# for i in word:
-#    if i in freq:
-#       freq[i]+=1
-#    else:
-#       freq[i]=1
-# print(freq)
-   
+word="MISSISSIPPI"
+freq={}
+for i in word:
+   if i in freq:
+      freq[i]+=1
+   else:
+      freq[i]=1
+print(freq)
+
+
+###
+word="MISSISSIPPI"
+dict={}
+i=0
+while i<len(word):
+   if word[i] in dict:
+      dict[word[i]]+=1
+   else:
+      dict[word[i]]=1
+      i+=1
+print(dict)
---
+++ logical.quest.py
@@ -0,0 +1,7 @@
+a=[["a",1],["p",2],["l",1],["e",1]]
+# n={k[0]:k[1] for k in a}
+# print(a)
+dict={}
+
+print(dict)
+   
